98ea0c8770fed534ab2050a78465e6c3bc9be9d0
xiaobai-V <805892347@qq.com>
2023-04-21 20:05:54 +0800

    bubble sort

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 print("hello git!")
+
+
+# 冒泡排序
+
+def bubble_sort(lists):
+    # 冒泡排序
+    count = len(lists)
+    for i in range(0, count):
+        for j in range(i + 1, count):
+            if lists[i] > lists[j]:
+                lists[i], lists[j] = lists[j], lists[i]
+    return lists
+
+
+# 测试
+if __name__ == "__main__":
+    lists = [3, 4, 2, 8, 9, 5, 1]
+    print(bubble_sort(lists))
